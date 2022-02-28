e7d20baa8d41393097926894495b90ab737c568b
Fernando Aragão <fernandoarag@gmail.com>
2022-02-28 14:53:37 -0300

    Adding deleteCategory in CategoryListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,12 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      res => this.categories = res,
+      err => console.error('Erro ao carregar a lista: ', err)
+    ).add(() => console.log('[GetAll][Categories]: Finally'));
   }
 
-  excluir() {
-    alert('Excluir');
+  deleteCategory(category: Category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element !== category),
+        err => console.error('Erro ao deletar categoria: ', err)
+      );
+    }
   }
 }
